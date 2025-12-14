e6248dc9c71034028cc063750f82de3fa3529fed
Tsukumo <contact@tsukumo.f5.si>
2025-12-14 23:34:52 +0900

    nix fmt

--- common/home-manager/desktop/default.nix
+++ common/home-manager/desktop/default.nix
@@ -10,7 +10,7 @@
     ./activate-linux.nix
     ./niri.nix
   ];
- programs.fuzzel = {
+  programs.fuzzel = {
     enable = true;
     settings = {
       main = {
@@ -26,10 +26,12 @@
     settings = {
       main = {
         font = "Adwaita Mono Nerd Font:size=12";
-        include = toString (pkgs.fetchurl {
-          url = "https://codeberg.org/dnkl/foot/raw/commit/6e533231b016684a32a1975ce2e33ae3ae38b4c6/themes/catppuccin-latte";
-          hash = "sha256-kTrLlIhBLFpxHUlXHCaK2nyq/m15L1iQjNngo5gPfCE=";
-        });
+        include = toString (
+          pkgs.fetchurl {
+            url = "https://codeberg.org/dnkl/foot/raw/commit/6e533231b016684a32a1975ce2e33ae3ae38b4c6/themes/catppuccin-latte";
+            hash = "sha256-kTrLlIhBLFpxHUlXHCaK2nyq/m15L1iQjNngo5gPfCE=";
+          }
+        );
       };
       mouse = {
         hide-when-typing = true;
--- common/home-manager/desktop/niri.nix
+++ common/home-manager/desktop/niri.nix
@@ -1,8 +1,8 @@
 {
-config,
-pkgs,
-lib,
-...
+  config,
+  pkgs,
+  lib,
+  ...
 }:
 
 {
@@ -59,59 +59,59 @@ lib,
       binds =
         with config.lib.niri.actions;
         lib.mapAttrs
-        (_: action: {
-          inherit action;
-          repeat = false;
-        })
-        (
-          let
-            normalBind = {
-              "Shift+P" = power-off-monitors;
+          (_: action: {
+            inherit action;
+            repeat = false;
+          })
+          (
+            let
+              normalBind = {
+                "Shift+P" = power-off-monitors;
 
-              "Return" = spawn "${pkgs.foot}/bin/footclient";
-              "Space" = spawn "fuzzel";
-              # "Space" = spawn "anyrun";
-              "M" = spawn "chromium" "--new-window";
+                "Return" = spawn "${pkgs.foot}/bin/footclient";
+                "Space" = spawn "fuzzel";
+                # "Space" = spawn "anyrun";
+                "M" = spawn "chromium" "--new-window";
 
-              "H" = focus-column-left;
-              "L" = focus-column-right;
-              "K" = focus-window-up;
-              "J" = focus-window-down;
-              "Shift+H" = move-column-left;
-              "Shift+L" = move-column-right;
-              "Shift+K" = move-window-up;
-              "Shift+J" = move-window-down;
+                "H" = focus-column-left;
+                "L" = focus-column-right;
+                "K" = focus-window-up;
+                "J" = focus-window-down;
+                "Shift+H" = move-column-left;
+                "Shift+L" = move-column-right;
+                "Shift+K" = move-window-up;
+                "Shift+J" = move-window-down;
 
-              "Q" = close-window;
-              "F" = maximize-column;
-              "Shift+F" = fullscreen-window;
-              "O" = toggle-overview;
+                "Q" = close-window;
+                "F" = maximize-column;
+                "Shift+F" = fullscreen-window;
+                "O" = toggle-overview;
 
-              "P" =
-                spawn "sh" "-c"
-                "${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g - - | ${pkgs.wl-clipboard}/bin/wl-copy";
+                "P" =
+                  spawn "sh" "-c"
+                    "${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g - - | ${pkgs.wl-clipboard}/bin/wl-copy";
 
-              "A" =
-                spawn "sh" "-c"
-                "${pkgs.libnotify}/bin/notify-send \"$(date +%H:%M:%S)\" \"$(date +%Y/%m/%d)\n$(${pkgs.acpi}/bin/acpi -b | cut -d: -f2- | sed 's/^, //')\"";
-            };
-            worksp = builtins.listToAttrs (
-              map (n: {
-                name = toString n;
-                value = focus-workspace n;
-              }) (lib.range 0 9)
-            );
-            other = {
-              "XF86AudioRaiseVolume" = spawn "swayosd-client" "--output-volume" "raise";
-              "XF86AudioLowerVolume" = spawn "swayosd-client" "--output-volume" "lower";
-              "XF86AudioMute" = spawn "swayosd-client" "--output-volume" "mute-toggle";
-              "XF86AudioMicMute" = spawn "swayosd-client" "--input-volume" "mute-toggle";
-              "XF86MonBrightnessUp" = spawn "swayosd-client" "--brightness" "raise";
-              "XF86MonBrightnessDown" = spawn "swayosd-client" "--brightness" "lower";
-            };
-          in
+                "A" =
+                  spawn "sh" "-c"
+                    "${pkgs.libnotify}/bin/notify-send \"$(date +%H:%M:%S)\" \"$(date +%Y/%m/%d)\n$(${pkgs.acpi}/bin/acpi -b | cut -d: -f2- | sed 's/^, //')\"";
+              };
+              worksp = builtins.listToAttrs (
+                map (n: {
+                  name = toString n;
+                  value = focus-workspace n;
+                }) (lib.range 0 9)
+              );
+              other = {
+                "XF86AudioRaiseVolume" = spawn "swayosd-client" "--output-volume" "raise";
+                "XF86AudioLowerVolume" = spawn "swayosd-client" "--output-volume" "lower";
+                "XF86AudioMute" = spawn "swayosd-client" "--output-volume" "mute-toggle";
+                "XF86AudioMicMute" = spawn "swayosd-client" "--input-volume" "mute-toggle";
+                "XF86MonBrightnessUp" = spawn "swayosd-client" "--brightness" "raise";
+                "XF86MonBrightnessDown" = spawn "swayosd-client" "--brightness" "lower";
+              };
+            in
             (lib.mapAttrs' (key: lib.nameValuePair "Mod+${key}") (normalBind // worksp)) // other
-        );
+          );
       spawn-at-startup = [
         # fcitx5は、autostartにファイルがあるから不要っぽい
         {
