3f2173f7e6a31f9bdf1138c8497099a167a21c87
Tsukumo <contact@tsukumo.f5.si>
2025-12-17 16:41:00 +0900

    Thinkpadがブートできなくなるのを回避

--- hosts/thinkpadx13-nix/configuration.nix
+++ hosts/thinkpadx13-nix/configuration.nix
@@ -9,7 +9,7 @@
   # custom.desktop.steam.enable = true;
 
   imports = [
-    # ./hardware.nix
+    ./hardware.nix # 再インストール時に消す
   ];
 
   zramSwap = {
@@ -21,7 +21,7 @@
   custom = {
     secrets.secretKey = "/etc/age/key.txt";
     disko = {
-      enable = true;
+      # enable = true; # 後で再インストールするときに有効にする
       diskName =  "/dev/disk/by-id/nvme-SKHynix_HFS256GD9TNI-L2B0B_NY06N11541090762N";
       swapSize = "10G";
     };
