4d2d7faf0ad85625e0c5c30f902dc33d3e04537f
Tsukumo <contact@tsukumo.f5.si>
2025-12-19 03:42:52 +0900

    nix flake update

--- common/core/default.nix
+++ common/core/default.nix
@@ -1,12 +1,11 @@
 { lib, ... }:
 {
-  imports = (
+  imports =
     map (name: ./. + "/${name}") (
       builtins.attrNames (
         lib.filterAttrs (
           name: type: type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix"
         ) (builtins.readDir ./.)
       )
-    )
   );
 }
