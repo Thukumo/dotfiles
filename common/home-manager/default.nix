2718685e95e0954c57310e2a946cff93c5fe0f77
Tsukumo <contact@tsukumo.f5.si>
2025-12-18 21:35:23 +0900

    モジュールとして使うならそもそもユーザ名とかの設定要らないらしい

--- common/home-manager/default.nix
+++ common/home-manager/default.nix
@@ -1,11 +1,6 @@
 { ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "tsukumo";
-  home.homeDirectory = "/home/tsukumo";
-
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
