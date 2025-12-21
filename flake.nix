cbe52f8d71c0394a91c4050d2678f0ec4d45d3c3
Tsukumo <contact@tsukumo.f5.si>
2025-12-22 02:35:45 +0900

    antigravityを導入してみるなど(その内モジュールにする)

--- flake.nix
+++ flake.nix
@@ -30,6 +30,11 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    antigravity-nix = {
+      url = "github:jacopone/antigravity-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
--- hosts/thinkpadx13-nix/default.nix
+++ hosts/thinkpadx13-nix/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ antigravity-nix, ... }:
 
 {
   system = "x86_64-linux";
@@ -8,5 +8,18 @@
   modules = [
     ./configuration.nix
     ./hardware-configuration.nix
+    {
+      home-manager.users."tsukumo" =
+        { config, ... }:
+        {
+          home.packages = [
+            antigravity-nix.packages.x86_64-linux.default
+          ];
+          home.persistence."/persist${config.home.homeDirectory}".directories = [
+            ".antigravity"
+            ".config/Antigravity"
+          ];
+        };
+    }
   ];
 }
