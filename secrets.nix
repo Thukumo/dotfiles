7e24ddb3e9390ae497cfdea15735cf1da9042a22
Tsukumo <contact@tsukumo.f5.si>
2025-12-22 11:29:59 +0900

    add gf65 final 2025

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,6 @@
 let
   keys = {
-    gf65 = "age1dt60h65uamutv5je4n2q554yuvv7npcnrh0y76hfrc3ve050v3mqnn6dpp";
+    gf65 = "age1pw34dvuy3k0pmpas7ej4sy2d8mutvp724a5afylfr0rcknndpyrscdzeh8";
     home_manager_key = "age1nzd7yc6dyg2m5ev35zdtydw8vprqx2qyt5pg6l5h786gqnge3vvsp6xxvh";
     backup-pixel9a = "age1akl70p6av6sjhuqa8wrr9ms5vn0jy6kgn5vh35c9m0jmg6hlrqtq9hp4cm";
     thinkpadx13-gen1 = "age1y3w68vz3g24mcaqu42vg76q0p9urnjekn42p60nlxnkh2zgdwqfsm4txkl";
