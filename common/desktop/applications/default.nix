bbd60d49c337fa43d1b78d438de4f1b234634d26
tsukumo <contact@tsukumo.f5.si>
2025-12-30 02:37:34 +0900

    steamの移動

--- common/desktop/applications/default.nix
+++ common/desktop/applications/default.nix
@@ -1,14 +1,12 @@
 { lib, ... }:
 {
-  imports =
-    (map (name: ./. + "/${name}") (
+  imports = (
+    map (name: ./. + "/${name}") (
       builtins.attrNames (
         lib.filterAttrs (
           name: type: type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix"
         ) (builtins.readDir ./.)
       )
-    ))
-    ++ [
-      ./steam
-    ];
+    )
+  );
 }
