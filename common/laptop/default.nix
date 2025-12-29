9dfd694079256074b55a60ad29c940bd8c8fa7a9
tsukumo <contact@tsukumo.f5.si>
2025-12-30 02:20:21 +0900

    wip: auto-cpufreq

--- common/laptop/default.nix
+++ common/laptop/default.nix
@@ -6,14 +6,6 @@
     default = false;
   };
   config = lib.mkIf config.custom.laptop.enable {
-    services.tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "balance_performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
-      };
-    };
+    services.auto-cpufreq.enable = true;
   };
 }
