1c4c80918f0f048574593ac4c536746be9630fb0
tsukumo <contact@tsukumo.f5.si>
2026-01-02 16:37:58 +0900

    boot周り

--- common/core/boot.nix
+++ common/core/boot.nix
@@ -7,6 +7,16 @@
     timeout = 0;
   };
 
+  boot.kernelParams = [
+    "quiet"
+    "loglevel=3"
+    "rd.udev.log_level=3"
+    "rd.systemd.show_status=false"
+  ];
+  boot.consoleLogLevel = 0;
+
+  # boot.initrd.systemd.enable = true;
+
   boot.kernel.sysctl."kernel.sysrq" = 242;
 
   boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
