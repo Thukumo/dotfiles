a358d0d18cd51af96e26c2affc2ae72bfcaacdb8
tsukumo <contact@tsukumo.f5.si>
2026-01-18 04:33:50 +0900

    pipewireのバッファサイズを増やした

--- common/desktop/pipewire.nix
+++ common/desktop/pipewire.nix
@@ -7,6 +7,15 @@
 {
   config = lib.mkIf config.custom.desktop.pipewire.enable {
     security.rtkit.enable = true;
-    services.pipewire.enable = true;
+    services.pipewire = {
+      enable = true;
+      extraConfig.pipewire."99-buffer-fix" = {
+        "context.properties" = {
+          "default.clock.quantum" = 1024;
+          "default.clock.min-quantum" = 1024;
+          "default.clock.max-quantum" = 2048;
+        };
+      };
+    };
   };
 }
