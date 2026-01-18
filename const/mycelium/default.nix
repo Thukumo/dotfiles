420243539213071717f733025e99a4d89200486c
tsukumo <contact@tsukumo.f5.si>
2026-01-18 15:35:49 +0900

    fix hostname

--- const/mycelium/default.nix
+++ const/mycelium/default.nix
@@ -3,7 +3,7 @@
 {
   networking.hosts = {
     "482:d00b:576e:de40:7f7a:564f:515f:cb7a" = [
-      "f51-b"
+      "f-51b"
       "termux"
     ];
     "4d8:de15:85dd:7fa4:493e:f1a4:6961:bd9c" = [ "thinkpad-x13-nix" ];
