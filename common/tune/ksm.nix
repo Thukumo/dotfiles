3c0a0f6185181f703304747f872ff62e8172b27c
tsukumo <contact@tsukumo.f5.si>
2026-02-07 03:07:05 +0900

    add options.custom.tune.ksm.enableForAll

--- common/tune/ksm.nix
+++ common/tune/ksm.nix
@@ -1,12 +1,24 @@
 {
   config,
   myLib,
+  lib,
   ...
 }:
 {
-  options.custom.tune.ksm.enable = myLib.mkEnabledOption;
+  options.custom.tune.ksm = {
+    enable = myLib.mkEnabledOption;
+    enableForAll = lib.mkEnableOption "Enable ksm for all";
+  };
 
-  config = {
-    hardware.ksm.enable = config.custom.tune.ksm.enable;
+  config = lib.mkIf config.custom.tune.ksm.enable {
+    hardware.ksm.enable = true;
+    # https://github.com/cachyos/cachyos-pkgbuilds/tree/master/cachyos-ksm-settings
+    environment.etc."systemd/system/service.d/10-ksm.conf" =
+      lib.mkIf config.custom.tune.ksm.enableForAll {
+        text = ''
+          [Service]
+          MemoryKSM=yes
+        '';
+      };
   };
 }
