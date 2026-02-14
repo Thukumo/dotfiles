ba30fa285a2a15a8649cd4d12815d58185516150
tsukumo <contact@tsukumo.f5.si>
2026-02-14 19:26:59 +0900

    pipewire なんかいじってみた

--- common/desktop/pipewire.nix
+++ common/desktop/pipewire.nix
@@ -9,11 +9,30 @@
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
-      extraConfig.pipewire."99-buffer-fix" = {
-        "context.properties" = {
-          "default.clock.quantum" = 1024;
-          "default.clock.min-quantum" = 1024;
-          "default.clock.max-quantum" = 2048;
+      extraConfig.pipewire = {
+        "99-buffer-fix" = {
+          "context.properties" = {
+            "default.clock.quantum" = 1024;
+            "default.clock.min-quantum" = 1024;
+            "default.clock.max-quantum" = 2048;
+          };
+        };
+        "99-high-res" = {
+          "context.properties" = {
+            # "default.clock.rate" = 192000;
+            "default.clock.allowed-rates" = [
+              44100
+              48000
+              88200
+              96000
+              176400
+              192000
+            ];
+          };
+          "stream.properties" = {
+            "resample.quality" = 10;
+            "channelmix.normalize" = false;
+          };
         };
       };
     };
