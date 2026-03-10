0fb17d00f7d18fdaa2b7fbd822f7e12272493895
tsukumo <contact@tsukumo.f5.si>
2026-03-11 04:17:56 +0900

    fix: use myLib.mkEnabledOption for fwupdmgr

--- common/hardware/firmware/fwupdmgr.nix
+++ common/hardware/firmware/fwupdmgr.nix
@@ -1,11 +1,12 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  myLib,
+  ...
+}:
 
 {
-  options.custom.hardware.fwupdmgr.enable = lib.mkOption {
-    type = lib.types.bool;
-    default = true;
-    description = "Whether to enable fwupdmgr.";
-  };
+  options.custom.hardware.fwupdmgr.enable = myLib.mkEnabledOption;
 
   config = lib.mkIf config.custom.hardware.fwupdmgr.enable {
     services.fwupd.enable = true;
