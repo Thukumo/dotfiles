a7f1460524cb3b4fa9223e4ef5d990145baee693
tsukumo <contact@tsukumo.f5.si>
2026-03-13 22:05:16 +0900

    zsh: add fish-like plugins and improve completions

--- common/shell/zsh.nix
+++ common/shell/zsh.nix
@@ -11,7 +11,7 @@
 
     enableCompletion = true;
     autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+    # syntaxHighlighting.enable = true; # fast-syntax-highlightingを使うため無効化
 
     history = {
       size = 10000;
@@ -20,13 +20,29 @@
       ignoreDups = true;
     };
 
+    plugins = [
+      {
+        name = "fast-syntax-highlighting";
+        src = pkgs.zsh-fast-syntax-highlighting;
+        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+      }
+      {
+        name = "zsh-history-substring-search";
+        src = pkgs.zsh-history-substring-search;
+        file = "share/zsh-history-substring-search/zsh-history-substring-search.zsh";
+      }
+    ];
+
     initContent = ''
       # Add zsh-completions to fpath before compinit
-      fpath+=( ${pkgs.zsh-completions}/share/zsh-completions )
+      fpath+=(
+        ${pkgs.zsh-completions}/share/zsh-completions
+        ${pkgs.nix-zsh-completions}/share/zsh/site-functions
+      )
 
-      # fish-like behavior for up/down arrows
-      bindkey '^[[A' up-line-or-search
-      bindkey '^[[B' down-line-or-search
+      # history-substring-search bindings (fish-like)
+      bindkey '^[[A' history-substring-search-up
+      bindkey '^[[B' history-substring-search-down
 
       # Case-insensitive completion
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
