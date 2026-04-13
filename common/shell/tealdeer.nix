cbbe852a9dcc88dc845b34322ef8bbcc46acfeca
tsukumo <contact@tsukumo.f5.si>
2026-04-13 16:12:30 +0900

    tealdeer: auto update( and persist)

--- common/shell/tealdeer.nix
+++ common/shell/tealdeer.nix
@@ -1,7 +1,16 @@
-_:
-
 {
   programs.tealdeer = {
     enable = true;
+    settings = {
+      updates = {
+        auto_update = true;
+      };
+    };
+  };
+
+  home.persistence."/persist" = {
+    directories = [
+      ".cache/tealdeer"
+    ];
   };
 }
