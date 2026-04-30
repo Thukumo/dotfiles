2a4d23f4dc52ff77ff0c36c2d010aa06dfd5fef6
tsukumo <contact@tsukumo.f5.si>
2026-04-30 23:21:06 +0900

    不要な回避策を削除

--- common/network/cloudflare-warp.nix
+++ common/network/cloudflare-warp.nix
@@ -10,7 +10,5 @@
 
   config = lib.mkIf config.custom.network.cloudflare-warp.enable {
     services.cloudflare-warp.enable = true;
-    # https://github.com/NixOS/nixpkgs/pull/515001
-    boot.kernel.sysctl."net.ipv4.conf.all.src_valid_mark" = 1;
   };
 }
