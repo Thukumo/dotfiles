310c430e4d08d28bb1abaf5c6e4deb5a668fedf9
tsukumo <contact@tsukumo.f5.si>
2026-05-09 15:40:33 +0900

    add mouse-3(mycelium addr)

--- const/mycelium/default.nix
+++ const/mycelium/default.nix
@@ -6,6 +6,7 @@
       "f-51b"
     ];
     "4d8:de15:85dd:7fa4:493e:f1a4:6961:bd9c" = [ "thinkpad-x13-nix" ];
+    "40f:4972:ada8:df1b:14d5:bfe:b7de:961d" = [ "mouse-3" ];
   };
   home-manager.users = myLib.mkForEachUsers (_: true) {
     programs.ssh.matchBlocks = {
