f5ba6eb874451491707a9e41afcfaf44015dd0c4
tsukumo <contact@tsukumo.f5.si>
2026-07-03 17:33:34 +0900

    add tobira.nvim

--- common/shell/nixvim/extra.nix
+++ common/shell/nixvim/extra.nix
@@ -13,9 +13,19 @@
           };
         };
       }
+      {
+        plugin = pkgs.vimUtils.buildVimPlugin {
+          name = "tobira.nvim";
+          src = builtins.fetchGit {
+            url = "https://github.com/kamegoro/tobira.nvim";
+            rev = "cd286ca662703b9d9082704892995565aac9f048";
+          };
+        };
+      }
     ];
     extraConfigLua = ''
       ${builtins.readFile ./luaconfig/accelerated-jk.luaconfig}
+      ${builtins.readFile ./luaconfig/tobira.luaconfig}
     '';
   };
 }
