946b23ad1a7ca81535b2ec2e40e42dd2ab0e62be
tsukumo <contact@tsukumo.f5.si>
2026-07-09 09:23:28 +0900

    わざわざハードコートする必要なかったね

--- flake.nix
+++ flake.nix
@@ -183,9 +183,8 @@
                   };
                 };
               };
-              # ホームディレクトリにあるキーペアと同一
               users.users.nixos.openssh.authorizedKeys.keys = [
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbmCSnxi4i+LHKTtZsX++GocB95+Px+uMGC0rywgiXe tsukumo@thinkpadx13-nix"
+                (lib.trim (builtins.readFile ./common/shell/ssh/id_ed25519.pub))
               ];
               services.avahi = {
                 enable = true;
