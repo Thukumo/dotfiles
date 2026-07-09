0e2d1557db72bfec45e2c153a11915bb26f07750
tsukumo <contact@tsukumo.f5.si>
2026-07-09 09:33:44 +0900

    キャッシュサーバとしてniri.cachix.orgを追加

--- common/desktop/de/niri/default.nix
+++ common/desktop/de/niri/default.nix
@@ -6,16 +6,26 @@
 }:
 
 {
-  config = {
-    custom.desktop.sessionCommand = lib.mkIf (
-      config.custom.desktop.anyEnabled
-      && (lib.any (u: u.desktop.enable && u.desktop.de == "niri") (lib.attrValues config.custom.users))
-    ) "niri-session";
+  config =
+    lib.mkIf
+      (
+        config.custom.desktop.anyEnabled
+        && (lib.any (u: u.desktop.enable && u.desktop.de == "niri") (lib.attrValues config.custom.users))
+      )
+      {
+        custom.desktop.sessionCommand = "niri-session";
 
-    home-manager.users = desktopLib.mkHome (user: user.custom.desktop.de or null == "niri") (_: {
-      imports = [
-        ./home.nix
-      ];
-    });
-  };
+        nix.settings = {
+          substituters = [ "https://niri.cachix.org" ];
+          trusted-public-keys = [
+            "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+          ];
+        };
+
+        home-manager.users = desktopLib.mkHome (user: user.custom.desktop.de or null == "niri") (_: {
+          imports = [
+            ./home.nix
+          ];
+        });
+      };
 }
