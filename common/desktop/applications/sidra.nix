e722bd03e1211bd0ddcc858058fba87aeeb0a219
tsukumo <contact@tsukumo.f5.si>
2026-08-03 04:21:11 +0900

    nix flake update, PR取り込まれてた

--- common/desktop/applications/sidra.nix
+++ common/desktop/applications/sidra.nix
@@ -4,18 +4,11 @@
   pkgs,
   ...
 }:
-let
-  inherit ((builtins.fromJSON (builtins.readFile "${inputs.sidra}/package.json"))) version;
-  # sidra is shipped as a prebuilt deb (CastLabs VMP-signed for Widevine);
-  # we repackage it and patch the bundled app.asar to fall back to
-  # getShareUrl() for xesam:url (see sidra-linux.nix)
-  sidra = pkgs.callPackage ./sidra/linux.nix { inherit version; };
-in
 {
   config = {
     home-manager.users = desktopLib.mkHome (user: user.custom.desktop.apps.sidra.enable or false) (
       _: _: {
-        home.packages = [ sidra ];
+        home.packages = [ inputs.sidra.packages.${pkgs.stdenv.hostPlatform.system}.default ];
         home.persistence."/persist".directories = [
           ".config/Sidra"
         ];
