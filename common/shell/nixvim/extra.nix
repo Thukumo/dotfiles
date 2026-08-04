e1c8cf6d6842c616588f9ed8d001485d252d04c3
tsukumo <contact@tsukumo.f5.si>
2026-08-04 22:27:27 +0900

    fetchFromGitHubに

--- common/shell/nixvim/extra.nix
+++ common/shell/nixvim/extra.nix
@@ -7,18 +7,22 @@
       {
         plugin = pkgs.vimUtils.buildVimPlugin {
           name = "accelerated-jk.nvim";
-          src = builtins.fetchGit {
-            url = "https://github.com/rainbowhxch/accelerated-jk.nvim";
+          src = pkgs.fetchFromGitHub {
+            owner = "rainbowhxch";
+            repo = "accelerated-jk.nvim";
             rev = "8fb5dad4ccc1811766cebf16b544038aeeb7806f";
+            hash = "sha256-zpjqCARlQU6g50s8wpaqN9xFK4tdUbrxU6MJrQZfSA8=";
           };
         };
       }
       {
         plugin = pkgs.vimUtils.buildVimPlugin {
           name = "tobira.nvim";
-          src = builtins.fetchGit {
-            url = "https://github.com/kamegoro/tobira.nvim";
+          src = pkgs.fetchFromGitHub {
+            owner = "kamegoro";
+            repo = "tobira.nvim";
             rev = "cd286ca662703b9d9082704892995565aac9f048";
+            hash = "sha256-V5Q+uK8X37CYG0nZfJSUnRfKoZsOF/CWrQYY8dw4f7I=";
           };
         };
       }
